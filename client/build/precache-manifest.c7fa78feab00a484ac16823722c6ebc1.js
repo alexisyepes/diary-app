d4df6d3333d866b7d547c5d3d2975791bcd7fc6c
Alex Sanabria <alexisyep@gmail.com>
2020-01-11 23:34:18 -0500

    build

---
+++ client/build/precache-manifest.c7fa78feab00a484ac16823722c6ebc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d377789f509ab009e058",
+    "url": "/static/js/main.71af2d88.chunk.js"
+  },
+  {
+    "revision": "725231ad8514096e4bff",
+    "url": "/static/js/2.38609d69.chunk.js"
+  },
+  {
+    "revision": "d377789f509ab009e058",
+    "url": "/static/css/main.71a46469.chunk.css"
+  },
+  {
+    "revision": "e282d715074b167ca517d67135ab8153",
+    "url": "/index.html"
+  }
+];
